89da1028159d82cc76330c25a89652e8638db8c7
DrewAdams85 <thebigpurchase@gmail.com>
2015-04-13 21:51:10 -0600

    Update Job.java

--- BusinessManager/Job.java
+++ BusinessManager/Job.java
@@ -1,3 +1,43 @@
+//Created by Kent Allen and Drew Adams - 4/01/2015
 public class Job {
-   
+	
+	// *** declaring variables ***
+	private String title;
+	private double price, hours;
+	private int numberOfWorkers;
+	
+	// *** constructor ***
+	public Job(String title, double price) {
+		this.title = title;
+		this.price = price;
+	}
+	
+	//  *** methods ***
+	
+	// getters
+	public double getHours() {
+		return fName;
+	}
+	
+	public int getNumberOfWorkers() {
+		return numberOfWorkers;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+
+	public string getTitle() {
+		return title;
+	}
+	
+	// setters
+	public void setHours(double hours) {
+		this.hours = hours;
+	}
+	
+	public void setNumberOfWorkers(int numberOfWorkers) {
+		this.numberOfWorkers = numberOfWorkers;
+	}
+		
 }
