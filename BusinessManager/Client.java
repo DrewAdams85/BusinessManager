e37f0ddb9cd9aad75e4eb51ad6702e90f4d9982c
DrewAdams85 <thebigpurchase@gmail.com>
2015-04-13 21:52:07 -0600

    Update Client.java
    Added notes

--- BusinessManager/Client.java
+++ BusinessManager/Client.java
@@ -1,43 +1,51 @@
+//Created by Kent Allen and Drew Adams - 4/01/2015
 public class Client {
-   private String fName, lName, address, phone;
-   
-   public Client(String fName, String lName, String address, String phone) {
-      this.fName = fName;
-      this.lName = lName;
-      this.address = address;
-      this.phone = phone;
-   }
-   
-   public String getFName() {
-      return fName;
-   }
-   
-   public void setFName(String fName) {
-      this.fName = fName;
-   }
-   
-   public String getLName() {
-      return lName;
-   }
-   
-   public void setLName(String LName) {
-      this.lName = lName;
-   }
-   
-   public String address() {
-      return address;
-   }
-   
-   public void address(String address) {
-      this.address = address;
-   }
-   
-   public String phone() {
-      return phone;
-   }
-   
-   public void phone(String phone) {
-      this.phone = phone;
-   }
-   
+	
+	// *** declaring variables ***
+	private String fName, lName, address, phone;
+
+	// *** constructor ***
+	public Client(String fName, String lName, String address, String phone) {
+		this.fName = fName;
+		this.lName = lName;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	// *** methods ***
+
+	// getters
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getFName() {
+		return fName;
+	}
+
+	public String getLName() {
+		return lName;
+	}
+
+	// setters
+	public void setFName(String fName) {
+		this.fName = fName;
+	}
+
+	public void setLName(String lName) {
+		this.lName = lName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 }
