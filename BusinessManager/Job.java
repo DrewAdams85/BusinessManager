123e6b4775c3f93cda977ae7bf52b4fcd15dc891
DrewAdams85 <thebigpurchase@gmail.com>
2015-04-22 23:45:10 -0600

    added setters and getters
    ^

--- BusinessManager/Job.java
+++ BusinessManager/Job.java
@@ -1,34 +1,42 @@
-/************************************
-* Created by Kent Allen and Drew Adams
-* 4/01/2015
-************************************/
+import java.util.ArrayList;
 
-// Instance class
+/************************************
+ * Created by Kent Allen and Drew Adams 4/01/2015
+ ************************************/
 public class Job {
-	
-	// *** fields ***
+
+	// *** declaring variables ***
 	private String title, date;
-	private double price, hours;
-	
+	private double price, hours, pricePerHour, totalHours;
+	private ArrayList<Job> jobsList = new ArrayList<>();
+
 	// *** constructors ***
 	public Job(String date, String title, double price) {
-      this.date = date;
+		this.date = date;
 		this.title = title;
 		this.price = price;
 	}
-   
-   public Job(String date, String title, double pricePerHour, double totalHours) {
-      this.date = date;
-      this.title = title;
-      price = pricePerHour;
-      hours = totalHours;
-   }
-	
-	//  *** methods ***
-	
-	// getters
+
+	public Job(String date, String title, double pricePerHour, double totalHours) {
+		this.date = date;
+		this.title = title;
+		price = pricePerHour;
+		hours = totalHours;
+	}
+
+	// *** methods ***
+
+	// getter
 	public double getHours() {
 		return hours;
+	}
+	
+	public double getTotalHours() {
+		return totalHours;
+	}
+
+	public double getPricePerHour() {
+		return pricePerHour;
 	}
 	
 	public double getPrice() {
@@ -38,13 +46,38 @@ public class Job {
 	public String getTitle() {
 		return title;
 	}
-   
-   public String getDate() {
-      return date;
-   }
-	
+
+	public String getDate() {
+		return date;
+	}
+
+	public ArrayList<Job> getJobList() {
+		return jobsList;
+	}
+
 	// setters
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public void setPricePerHour(double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	public void setHours(double hours) {
 		this.hours = hours;
+	}
+	
+	public void setTotalHours(double totalHours) {
+		this.totalHours = totalHours;
 	}
 }
